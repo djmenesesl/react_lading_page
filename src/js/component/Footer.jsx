2b877aeb4ef884e78b8f6f8764874a7a087138e0
djmenesesl <dmenesesl08@gmail.com>
2022-08-29 03:14:07 +0000

    Arreglado el footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 export const Footer = (props) => {
     return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid d-flex">
-          
-        <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Copyright <i class="fa-solid fa-copyright text-light"></i> Your Website 2019</a>
-            </li>
-          </ul>
+    <footer className="footer mt-auto bg-dark py-3">
+        <div className="container-footer">
+          <span className="text-light text-center d-flex justify-content-center align-items-center">Copyright <i className="fa-solid fa-copyright text-light mx-2"></i> Your Website 2019</span>
         </div>
-      </div>
-    </nav>
+    </footer>
+     
     )
     
-    }
+  }
